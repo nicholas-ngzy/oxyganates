de522d97b29369d7aa69e5ac9cbc2944d8e47696
NicholasNgZY <nicholasngzy@gmail.com>
2021-12-26 13:32:43 +0800

    Fixed validation and error messages

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,72 +3,99 @@ import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import './Login.css';
+import '../App.css';
+import NavMenu from './NavMenu.js';
 
 const Login = ({ setToken }) => {
   const navigate = useNavigate();
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-  });
-
-  const validateForm = () => {
-    return user.email.length > 0 && user.password.length >= 8;
-  };
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUser({
-      ...user, //spread operator
+    setForm({
+      ...form, //spread operator
       [name]: value,
     });
+    // clear error
+    setErrors({
+      ...errors,
+      [name]: null,
+    });
+  };
+
+  const findError = () => {
+    const { email, password } = form;
+    const newErrors = {};
+    if (!email || email === '') newErrors.email = 'Required field';
+    if (!password || password === '') newErrors.password = 'Required field';
+    return newErrors;
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    try {
-      login();
-    } catch (e) {
-      alert(e.message);
+    const newErrors = findError();
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      try {
+        login();
+      } catch (e) {
+        console.log(e.message);
+      }
     }
   };
 
   const login = () => {
     axios
-      .post('http://localhost:6969/api/v1/login', user)
+      .post('http://localhost:6969/api/v1/login', form)
       .then((res) => {
         alert(res.data.message);
         setToken(res.data.token);
         localStorage.setItem('token', res.data.token);
-        navigate('/homepage');
+        navigate('/');
       })
       .catch((err) => {
+        if (err.response.status == '400') alert('Wrong credentials');
+        if (err.response.status == '404') alert('User not found');
         console.log(err);
       });
   };
 
   return (
     <div className='login-wrapper'>
+      <NavMenu />
       <h1>Log in</h1>
       <br />
-      <h4>Please enter your email and password</h4>
-      <br />
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size='lg' controlId='email'>
+      <h4>Please enter email and password</h4>
+      <Form onSubmit={handleSubmit} className='my-4'>
+        <Form.Group size='lg' controlId='email' className='my-4'>
           <Form.Label>Email</Form.Label>
-          <Form.Control autoFocus name='email' type='email' value={user.email} onChange={handleChange} />
+          <Form.Control
+            autoFocus
+            name='email'
+            type='email'
+            value={form.email}
+            onChange={handleChange}
+            isInvalid={!!errors.email}
+          />
+          <Form.Control.Feedback type='invalid'>{errors.email}</Form.Control.Feedback>
         </Form.Group>
-        <br />
-        <Form.Group size='lg' controlId='password'>
+        <Form.Group size='lg' controlId='password' className='my-4'>
           <Form.Label>Password</Form.Label>
-          <Form.Control name='password' type='password' value={user.password} onChange={handleChange} />
+          <Form.Control
+            name='password'
+            type='password'
+            value={form.password}
+            onChange={handleChange}
+            isInvalid={!!errors.password}
+          />
+          <Form.Control.Feedback type='invalid'>{errors.password}</Form.Control.Feedback>
         </Form.Group>
-        <br />
-        <Button block size='lg' type='submit' disabled={!validateForm}>
+        <Button block size='lg' type='submit'>
           Login
         </Button>
       </Form>
-      <br />
       Don't have an account? <Link to='/register'>Sign up</Link>
     </div>
   );
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -4,99 +4,142 @@ import { useNavigate, Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import FormGroup from 'react-bootstrap/esm/FormGroup';
-import FormLabel from 'react-bootstrap/esm/FormLabel';
-import './Login.css';
+import '../App.css';
+import NavMenu from './NavMenu';
 
 const Register = () => {
   const navigate = useNavigate();
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password1: '',
-    password2: '',
-    phone: '',
-  });
-
-  const validateForm = () => {
-    if (
-      user.email.length < 1 &&
-      user.name.length < 1 &&
-      user.password1.length < 8 &&
-      user.password2.length < 8 &&
-      user.phone.length < 10
-    ) {
-      return alert('Please enter all fields');
-    } else return 1;
-  };
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUser({
-      ...user,
+    setForm({
+      ...form,
       [name]: value,
     });
+    // clear error
+    setErrors({
+      ...errors,
+      [name]: null,
+    });
+  };
+
+  const findError = () => {
+    const { email, name, password1, password2, phone } = form;
+    const newErrors = {};
+    if (!email || email === '') newErrors.email = 'Required field';
+    if (!name || name === '') newErrors.name = 'Required field';
+    if (!password1 || password1 === '') newErrors.password1 = 'Required field';
+    if (password1 != password2) newErrors.password2 = 'Does not match';
+    if (!password2 || password2 === '') newErrors.password2 = 'Required field';
+    if (!phone || phone === '') newErrors.phone = 'Required field';
+    if (password1) {
+      if (password1.length < 8) newErrors.password1 = 'Minimum 8 characters';
+    }
+    if (password2) {
+      if (password2.length < 8) newErrors.password2 = 'Minimum 8 characters';
+    }
+    if (phone) {
+      if (phone.length < 10) newErrors.phone = 'Invalid phone number';
+    }
+    return newErrors;
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    try {
-      register();
-    } catch (e) {
-      alert(e.message);
+    const newErrors = findError();
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      try {
+        register();
+      } catch (e) {
+        console.log(e.message);
+      }
     }
   };
 
   const register = () => {
     axios
-      .post('http://localhost:6969/api/v1/register', user)
+      .post('http://localhost:6969/api/v1/register', form)
       .then((res) => {
         alert(res.data.message);
         navigate('/login');
       })
       .catch((err) => {
-        alert(err);
+        if (err.response.status == '403') alert('Email already exist');
       });
   };
 
   return (
     <div className='register-wrapper'>
+      <NavMenu />
       <h1>Sign up</h1>
       <br />
       <h4>Please enter all details</h4>
-      <br />
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size='lg' controlId='email'>
+      <Form onSubmit={handleSubmit} className='my-4'>
+        <Form.Group size='lg' controlId='email' className='my-4'>
           <Form.Label>Email</Form.Label>
-          <Form.Control autoFocus name='email' type='email' value={user.email} onChange={handleChange} />
+          <Form.Control
+            autoFocus
+            name='email'
+            type='email'
+            value={form.email}
+            onChange={handleChange}
+            isInvalid={!!errors.email}
+          />
+          <Form.Control.Feedback type='invalid'>{errors.email}</Form.Control.Feedback>
         </Form.Group>
-        <br />
-        <FormGroup size='lg' controlId='name'>
+
+        <FormGroup size='lg' controlId='name' className='my-4'>
           <Form.Label>Name</Form.Label>
-          <Form.Control name='name' type='text' value={user.name} onChange={handleChange} />
+          <Form.Control name='name' type='text' value={form.name} onChange={handleChange} isInvalid={!!errors.name} />
+          <Form.Control.Feedback type='invalid'>{errors.name}</Form.Control.Feedback>
         </FormGroup>
-        <br />
-        <Form.Group size='lg' controlId='password1'>
+
+        <Form.Group size='lg' controlId='password1' className='my-4'>
           <Form.Label>Password</Form.Label>
-          <Form.Control name='password1' type='password' value={user.password1} onChange={handleChange} />
+          <Form.Control
+            name='password1'
+            type='password'
+            value={form.password1}
+            onChange={handleChange}
+            isInvalid={!!errors.password1}
+          />
+          <Form.Control.Feedback type='invalid'>{errors.password1}</Form.Control.Feedback>
         </Form.Group>
-        <br />
-        <Form.Group size='lg' controlId='password2'>
+
+        <Form.Group size='lg' controlId='password2' className='my-4'>
           <Form.Label>Confirm password</Form.Label>
-          <Form.Control name='password2' type='password' value={user.password2} onChange={handleChange} />
+          <Form.Control
+            name='password2'
+            type='password'
+            value={form.password2}
+            onChange={handleChange}
+            isInvalid={!!errors.password2}
+          />
+          <Form.Control.Feedback type='invalid'>{errors.password2}</Form.Control.Feedback>
         </Form.Group>
-        <br />
-        <Form.Group size='lg' controlId='phone'>
+
+        <Form.Group size='lg' controlId='phone' className='my-4'>
           <Form.Label>Phone</Form.Label>
-          <Form.Control name='phone' type='text' value={user.phone} onChange={handleChange} />
+          <Form.Control
+            name='phone'
+            type='text'
+            value={form.phone}
+            onChange={handleChange}
+            isInvalid={!!errors.phone}
+          />
+          <Form.Control.Feedback type='invalid'>{errors.phone}</Form.Control.Feedback>
         </Form.Group>
-        <br />
-        <Button block size='lg' type='submit' disabled={!validateForm}>
+        <Button block size='lg' type='submit'>
           Register
         </Button>
       </Form>
-      <br />
       Already have an account?<Link to='/login'> Log in</Link>
     </div>
   );
 };
+
 export default Register;
