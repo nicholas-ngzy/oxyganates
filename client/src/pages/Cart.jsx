3decef02e324ea029fa7dcbb0e52ddbf07e2d7e8
NicholasNgZY <nicholasngzy@gmail.com>
2022-04-21 00:44:19 +0800

    Added payment and order module

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { Button, Card, CardActions, CardContent, CardHeader, Container, Grid, Typography } from '@mui/material';
+import Paypal from '../components/Paypal';
 
 export default function Cart() {
   const [cart, setCart] = useState([]);
-  const query = new URLSearchParams(useLocation().search);
-  const user = query.get('user');
+  const user = new URLSearchParams(useLocation().search).get('user');
   let subtotal = 0;
 
   useEffect(() => {
@@ -19,13 +19,12 @@ export default function Cart() {
       .catch((err) => {
         console.log(err);
       });
-  }, cart);
+  }, [user]);
 
   const handleClear = () => {
     axios
       .put(`http://localhost:6969/api/v1/cart?user=${user}`, [])
       .then((res) => {
-        alert(res.data.message);
         setCart([]);
       })
       .catch((err) => {
@@ -39,53 +38,53 @@ export default function Cart() {
         Cart
       </Typography>
       <Card>
-        <CardHeader title='Item' />
-        {cart.length === 0 && (
+        {cart.length === 0 ? (
+          <CardHeader title='Cart is empty' />
+        ) : (
           <CardContent>
-            <Typography>Cart is empty</Typography>
-          </CardContent>
-        )}
-        {cart.map((item) => {
-          subtotal += item.product.price * item.quantity;
-          return (
-            <CardContent key={item._id}>
+            {cart.map((item) => {
+              subtotal += item.product.price * item.quantity;
+              return (
+                <CardContent key={item._id}>
+                  <Grid container>
+                    <Grid item xs={6}>
+                      <Typography variant='h6'>{item.product.name}</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Typography variant='h6'>RM {(Math.round(item.product.price * 100) / 100).toFixed(2)}</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Typography variant='h6'>x {item.quantity}</Typography>
+                    </Grid>
+                    <Grid item xs={2}>
+                      <Typography variant='h6'>
+                        RM {(Math.round(item.product.price * item.quantity * 100) / 100).toFixed(2)}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              );
+            })}
+            <CardContent sx={{ display: 'flex', justifyContent: 'space-around' }}>
               <Grid container>
-                <Grid item xs={6}>
-                  <Typography variant='h6'>{item.product.name}</Typography>
+                <Grid item xs={10}>
+                  <Typography variant='h5'>Subtotal</Typography>
                 </Grid>
                 <Grid item xs={2}>
-                  <Typography variant='h6'>RM {(Math.round(item.product.price * 100) / 100).toFixed(2)}</Typography>
-                </Grid>
-                <Grid item xs={2}>
-                  <Typography variant='h6'>x {item.quantity}</Typography>
-                </Grid>
-                <Grid item xs={2}>
-                  <Typography variant='h6'>
-                    RM {(Math.round(item.product.price * item.quantity * 100) / 100).toFixed(2)}
-                  </Typography>
+                  <Typography variant='h5'>RM {(Math.round(subtotal * 100) / 100).toFixed(2)}</Typography>
                 </Grid>
               </Grid>
             </CardContent>
-          );
-        })}
-        <CardContent sx={{ display: 'flex', justifyContent: 'space-around' }}>
-          <Grid container>
-            <Grid item xs={10}>
-              <Typography variant='h5'>Subtotal</Typography>
-            </Grid>
-            <Grid item xs={2}>
-              <Typography variant='h5'>RM {(Math.round(subtotal * 100) / 100).toFixed(2)}</Typography>
-            </Grid>
-          </Grid>
-        </CardContent>
-        <CardActions>
-          <Button variant='contained' size='large'>
-            Checkout
-          </Button>
-          <Button variant='outlined' size='large' onClick={handleClear}>
-            Clear Cart
-          </Button>
-        </CardActions>
+            <CardActions>
+              <Button variant='outlined' size='large' onClick={handleClear}>
+                Clear Cart
+              </Button>
+            </CardActions>
+            <CardContent sx={{ textAlign: 'center' }}>
+              <Paypal cart={cart} subtotal={subtotal} user={user} handleClear={handleClear} />
+            </CardContent>
+          </CardContent>
+        )}
       </Card>
     </Container>
   );
--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -1,45 +1,86 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, CardContent, CardHeader, Chip, Collapse, Container, Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Chip, Container, Grid, Typography } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 
 export default function Order() {
   const [orders, setOrders] = useState([]);
+  const user = new URLSearchParams(useLocation().search).get('user');
 
   useEffect(() => {
     axios
-      .get('http://localhost:6969/api/v1/orders')
+      .get(`http://localhost:6969/api/v1/orders?user=${user}`)
       .then((res) => {
+        console.log(res.data);
         setOrders(res.data.orderList);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [user]);
 
   return (
     <Container>
       <Typography variant='h3' my={3} textAlign='center'>
         Order
       </Typography>
-      {orders.map((order) => {
-        return (
-          <Card raised key={order._id}>
-            <CardHeader title={order._id} subheader={convertDate(order.date)}></CardHeader>
-            <CardContent>
-              <Chip
-                label={order.status}
-                color={order.status === 'Success' ? 'success' : order.status === 'Processing' ? 'warning' : 'error'}
-              ></Chip>
-              <Typography>RM {order.total}</Typography>
-            </CardContent>
-            <CardContent>
-              <Collapse in={true}>
-                <Typography>{order.total}</Typography>
-              </Collapse>
-            </CardContent>
-          </Card>
-        );
-      })}
+      {orders.length === 0 ? (
+        <Card>
+          <CardHeader title='No order yet. Start shopping now' />
+        </Card>
+      ) : (
+        <Container>
+          {orders.map((order) => {
+            return (
+              <Card raised key={order._id}>
+                <CardHeader title={`Order ID: ${order._id}`} subheader={convertDate(order.date)} />
+                <CardContent>
+                  <Chip
+                    label={order.status}
+                    color={
+                      order.status === 'Completed' ? 'success' : order.status === 'Cancelled' ? 'error' : 'warning'
+                    }
+                  />
+                  {order.items.map((item) => {
+                    return (
+                      <CardContent key={item._id}>
+                        <Grid container>
+                          <Grid item xs={6}>
+                            <Typography variant='h6'>{item.product.name}</Typography>
+                          </Grid>
+                          <Grid item xs={2}>
+                            <Typography variant='h6'>
+                              RM {(Math.round(item.product.price * 100) / 100).toFixed(2)}
+                            </Typography>
+                          </Grid>
+                          <Grid item xs={2}>
+                            <Typography variant='h6'>x {item.quantity}</Typography>
+                          </Grid>
+                          <Grid item xs={2}>
+                            <Typography variant='h6'>
+                              RM {(Math.round(item.product.price * item.quantity * 100) / 100).toFixed(2)}
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </CardContent>
+                    );
+                  })}
+                  <CardContent sx={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <Grid container>
+                      <Grid item xs={10}>
+                        <Typography variant='h5'>Total</Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography variant='h5'>RM {(Math.round(order.total * 100) / 100).toFixed(2)}</Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </Container>
+      )}
     </Container>
   );
 }
