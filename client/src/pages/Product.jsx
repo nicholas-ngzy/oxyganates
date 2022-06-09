c764a93bac15b145b8ced82e0b1568d0e58bb104
NicholasNgZY <nicholasngzy@gmail.com>
2022-06-09 22:41:58 +0800

    refactor code

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,55 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Button, Container, Grid, TextField, Typography } from '@mui/material';
-import jwt_decode from 'jwt-decode';
+import TokenContext from '../context/TokenProvider';
 
 export default function Product() {
   const [item, setItem] = useState({});
   const [count, setCount] = useState(1);
   const { id } = useParams();
-  const [form, setForm] = useState({
-    product: id,
-    quantity: count,
-  });
+  const [form, setForm] = useState({ product: id });
   const navigate = useNavigate();
-  const token = localStorage.getItem('token') || null;
+  const { token, user } = useContext(TokenContext);
 
   useEffect(() => {
     axios
       .get(`http://localhost:6969/api/v1/products/${id}`)
-      .then((res) => {
-        setItem(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then((res) => setItem(res.data))
+      .catch((err) => console.log(err));
   }, []);
 
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setCount(value);
-    setForm({
-      ...form,
-      quantity: value,
-    });
+  const handleChange = (event) => {
+    const { value } = event.target;
+    if (value > item.quantity) alert(`Exceeded limit of ${item.quantity}`);
+    else if (value <= 0) alert(`Minimum quantity is 1`);
+    else if (!Number.isInteger(value)) alert(`Positive integer only`);
+    else {
+      setCount(value);
+      setForm({ ...form, quantity: value });
+    }
   };
+
   const handleClick = () => {
     if (token) {
-      const config = { headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' } };
-      const user = jwt_decode(localStorage.getItem('token')).userId;
-      axios
-        .patch(`http://localhost:6969/api/v1/cart?user=${user}`, form, config)
-        .then((res) => {
-          console.log(res.data);
-          alert(res.data.message);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else {
-      navigate('/login');
-    }
+      if (form.quantity <= item.quantity && form.quantity !== '') {
+        const config = { headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' } };
+        axios
+          .patch(`http://localhost:6969/api/v1/cart?user=${user.userId}`, form, config)
+          .then((res) => alert(res.data.message))
+          .catch((err) => console.log(err));
+      } else if (form.quantity > item.quantity) alert(`Exceeded limit of ${item.quantity}`);
+      else if (!Number.isInteger(form.quantity)) alert(`Positive integer only`);
+      else if (form.quantity <= 0) alert(`Minimum quantity is 1`);
+      else alert(`No quantity selected`);
+    } else navigate('/login');
   };
 
   return (
@@ -68,14 +61,8 @@ export default function Product() {
             type='number'
             name='quantity'
             onChange={handleChange}
-            InputProps={{
-              inputProps: {
-                min: 1,
-                max: item.quantity,
-                defaultValue: count,
-              },
-            }}
-          ></TextField>
+            InputProps={{ inputProps: { min: 1, max: item.quantity, defaultValue: count } }}
+          />
           <Button size='large' variant='contained' onClick={handleClick} sx={{ margin: '.5em' }}>
             Add to cart
           </Button>
