90f3dda0d91d4cc618491abed31ae4859af8b133
NicholasNgZY <nicholasngzy@gmail.com>
2022-06-10 17:23:48 +0800

    added stock decrement after purchase

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,40 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { Button, Card, CardActions, CardContent, CardHeader, Container, Grid, Typography } from '@mui/material';
 import Paypal from '../components/Paypal';
+import TokenContext from '../context/TokenProvider';
 
 export default function Cart() {
   const [cart, setCart] = useState([]);
-  const user = new URLSearchParams(useLocation().search).get('user');
+  const { user } = useContext(TokenContext);
   let subtotal = 0;
 
   useEffect(() => {
     axios
-      .get(`http://localhost:6969/api/v1/cart?user=${user}`)
-      .then((res) => {
-        console.log(res.data);
-        setCart(res.data.cart);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .get(`http://localhost:6969/api/v1/cart?user=${user.userId}`)
+      .then((res) => setCart(res.data.cart))
+      .catch((err) => console.log(err));
   }, [user]);
 
   const handleClear = () => {
     axios
-      .put(`http://localhost:6969/api/v1/cart?user=${user}`, [])
-      .then((res) => {
-        setCart([]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .put(`http://localhost:6969/api/v1/cart?user=${user.userId}`, [])
+      .then((res) => setCart([]))
+      .catch((err) => console.log(err));
   };
 
   return (
     <Container>
-      <Typography variant='h3' my={3} textAlign='center'>
+      <Typography variant='h4' py={3} textAlign='center'>
         Cart
       </Typography>
       <Card>
@@ -81,7 +72,7 @@ export default function Cart() {
               </Button>
             </CardActions>
             <CardContent sx={{ textAlign: 'center' }}>
-              <Paypal cart={cart} subtotal={subtotal} user={user} handleClear={handleClear} />
+              <Paypal cart={cart} subtotal={subtotal} user={user.userId} handleClear={handleClear} />
             </CardContent>
           </CardContent>
         )}
