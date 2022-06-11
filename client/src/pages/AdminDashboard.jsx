9d6f1b98cd3494b2b1feab0f4b1a2b2602bcb30e
NicholasNgZY <nicholasngzy@gmail.com>
2022-06-12 01:49:00 +0800

    added chart for report

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -3,11 +3,11 @@ import axios from 'axios';
 import { Grid, Typography, Table, TableContainer, TableHead, TableRow, TableCell } from '@mui/material';
 import NotFound from './NotFound';
 import TokenContext from '../context/TokenProvider';
-import { ComposedChart, Area, Bar, CartesianGrid, Tooltip, XAxis, YAxis, Legend } from 'recharts';
+import { ComposedChart, BarChart, Area, Bar, CartesianGrid, Tooltip, XAxis, YAxis, Legend } from 'recharts';
 import { TableBody } from '@mui/material';
 
 const EarningsChart = ({ data }) => (
-  <ComposedChart width={800} height={400} data={data} margin={{ top: 10, right: 10, left: 10, bottom: 10 }}>
+  <ComposedChart width={800} height={400} data={data}>
     <defs>
       <linearGradient id='colorArea' x1='0' y1='0' x2='0' y2='1'>
         <stop offset='5%' stopColor='#82ca9d' stopOpacity={0.8} />
@@ -15,20 +15,36 @@ const EarningsChart = ({ data }) => (
       </linearGradient>
     </defs>
     <XAxis dataKey='_id' />
-    <YAxis />
+    <YAxis yAxisId='left' label={{ value: 'Total sales', angle: -90, position: 'insideLeft' }} />
+    <YAxis yAxisId='right' orientation='right' label={{ value: 'Number of orders', angle: 90, position: 'reft' }} />
     <CartesianGrid strokeDasharray='3 3' />
     <Tooltip />
     <Legend />
     <Area
-      name='Total sales (RM)'
+      name='Total sales'
+      unit='RM'
+      yAxisId='left'
       type='monotone'
       dataKey='totalSaleAmount'
       stroke='#82ca9d'
       fillOpacity={1}
       fill='url(#colorArea)'
     />
-    <Bar name='Order count' dataKey='count' barSize={20} fill='#413ea0' />
+    <Bar name='Order count' dataKey='count' yAxisId='right' barSize={20} fill='#413ea0' />
   </ComposedChart>
+);
+
+const StatusChart = ({ data }) => (
+  <BarChart width={800} height={400} data={data}>
+    <XAxis dataKey='_id' />
+    <YAxis label={{ value: 'Number of orders', angle: -90, position: 'insideLeft' }} />
+    <CartesianGrid strokeDasharray='3 3' />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey='Completed' stackId='a' fill='#82ca9d' />
+    <Bar dataKey='Cancelled' stackId='a' fill='#8884d8' />
+    <Bar dataKey='Processing' stackId='a' fill='#ffc658' />
+  </BarChart>
 );
 
 const LowStockTable = ({ data }) => (
@@ -54,7 +70,8 @@ const LowStockTable = ({ data }) => (
 );
 
 export default function AdminDashboard() {
-  const [earningsData, setEarningsData] = useState({});
+  const [earningsData, setEarningsData] = useState();
+  const [statusData, setStatusData] = useState();
   const [countData, setCountData] = useState([]);
   const { user } = useContext(TokenContext);
 
@@ -62,6 +79,10 @@ export default function AdminDashboard() {
     axios
       .get(`http://localhost:6969/api/v1/orders/sales`)
       .then((res) => setEarningsData(res.data))
+      .catch((err) => console.log(err));
+    axios
+      .get(`http://localhost:6969/api/v1/orders/status`)
+      .then((res) => setStatusData(res.data))
       .catch((err) => console.log(err));
     axios
       .get(`http://localhost:6969/api/v1/products/count`)
@@ -76,17 +97,21 @@ export default function AdminDashboard() {
       </Typography>
       <Grid
         container
-        justifyContent='center'
-        alignItems='flex-start'
-        spacing={3}
+        direction='column'
+        justifyContent='flex-start'
+        alignItems='center'
+        spacing={5}
         width={'100%'}
         style={{ margin: '0 auto' }}
       >
-        <Grid item sm={12} md={4}>
-          <LowStockTable data={countData} />
-        </Grid>
-        <Grid item sm={12} md={8}>
+        <Grid item sm={12}>
           <EarningsChart data={earningsData} />
+        </Grid>
+        <Grid item sm={12}>
+          <StatusChart data={statusData} />
+        </Grid>
+        <Grid item sm={12}>
+          <LowStockTable data={countData} />
         </Grid>
       </Grid>
     </div>
